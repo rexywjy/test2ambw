f7629508d18c03be211e5ea688b3304150a5b125
rexywjy <rexywjy@gmail.com>
2024-05-24 19:16:42 +0700

    admin - customer folder

---
+++ test2ambw/lib/customer/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeCustomer extends StatelessWidget {
+  const HomeCustomer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- test2ambw/lib/main.dart
+++ test2ambw/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'customer/index.dart';
+import 'admin/index.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,12 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomeCustomer(),
+      // home: Scaffold(
+      //   body: Center(
+      //     child: Text('Hello World!'),
+      //   ),
+      // ),
     );
   }
 }
