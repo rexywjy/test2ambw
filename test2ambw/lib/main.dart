c91a7228ce7f7a4d2f6898a91e4b4ca11a843f0f
rexywjy <rexywjy@gmail.com>
2024-06-19 15:19:59 +0700

    supabase integrated

--- test2ambw/lib/main.dart
+++ test2ambw/lib/main.dart
@@ -7,11 +7,16 @@ import 'package:flutter/material.dart';
 import 'customer/index.dart';
 import 'admin/index.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: 'https://ydscvaytbbvwrofeojpj.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlkc2N2YXl0YmJ2d3JvZmVvanBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY1NTMyNTgsImV4cCI6MjAzMjEyOTI1OH0.R1YdW0VOhciEb6aO1ljiUT1iZrKilWoEoi4_K9xN_Y8',
   );
   runApp(MainApp());
 }
